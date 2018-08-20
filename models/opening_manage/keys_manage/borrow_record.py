1951ba4461dcba88658a9965867084fc9fe1c6f2
To_Yong <17711398613@163.com>
2018-08-20 15:38:55 +0800

    借用记录

--- models/opening_manage/keys_manage/borrow_record.py
+++ models/opening_manage/keys_manage/borrow_record.py
@@ -6,4 +6,16 @@ from odoo import models, fields, api
 class BorrowRecord(models.Model):
     _name = 'fuenc.station.borrow.record'
 
-    name = fields.Char(string='名字')
+    key_no = fields.Char(string='钥匙编号')
+    line = fields.Char(string='线路')
+    type = fields.Char(string='钥匙类型')
+    station = fields.Char(string='归属站点')
+    name = fields.Char(string='钥匙名称')
+    position = fields.Char(string='对应位置')
+    state = fields.Char(string='归还状态')
+    borrow_time = fields.Datetime(string='借用时间')
+    borrow_operate_member = fields.Char(string='借用操作人')
+    borrow_member = fields.Char(string='借用人')
+    return_time = fields.Datetime(string='归还时间')
+    return_operate_member = fields.Char(string='归还操作人')
+    return_member = fields.Char(string='归还人')
