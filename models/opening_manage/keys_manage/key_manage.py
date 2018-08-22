dbba44b133a2b0de0a8ceeae40ea7be9efe949bc
To_Yong <17711398613@163.com>
2018-08-22 14:21:05 +0800

    submit

--- models/opening_manage/keys_manage/key_manage.py
+++ models/opening_manage/keys_manage/key_manage.py
@@ -28,3 +28,44 @@ class KeyManage(models.Model):
 			# 'flags': {'initial_mode': 'edit'},
 			'target': 'new',
 		}
+	
+	# 借用钥匙
+	@api.model
+	def borrow_key(self):
+		return {
+			'name': '钥匙借用',
+			'type': 'ir.actions.act_window',
+			'view_type': 'form',
+			'view_mode': 'form',
+			'res_model': 'fuenc.station.borrow.record',
+			'context': self.env.context,
+			# 'flags': {'initial_mode': 'edit'},
+			'target': 'new',
+		}
+	
+	# 归还钥匙
+	@api.model
+	def return_key(self):
+		return {
+			'name': '钥匙归还',
+			'type': 'ir.actions.act_window',
+			'view_type': 'form',
+			'view_mode': 'form',
+			'res_model': 'fuenc.station.borrow.record',
+			'context': self.env.context,
+			# 'flags': {'initial_mode': 'edit'},
+			'target': 'new',
+		}
+	
+	# 借用记录
+	@api.model
+	def borrow_record(self):
+		view_tree = self.env.ref('fuenc_station.fuenc_station_borrow_record_list').id
+		view_form = self.env.ref('fuenc_station.fuenc_station_borrow_record_form').id
+		return {
+			'name': '借用记录',
+			"type": "ir.actions.act_window",
+			"res_model": "fuenc.station.borrow.record",
+			"views": [[view_tree, "tree"], [view_form, "form"]],
+			# "domain": [()],
+		}
