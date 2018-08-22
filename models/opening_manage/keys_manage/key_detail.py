1772b4e421c1c2bd63d936a82ac8551ee421ba39
To_Yong <17711398613@163.com>
2018-08-22 12:12:01 +0800

    创建钥匙

--- models/opening_manage/keys_manage/key_detail.py
+++ models/opening_manage/keys_manage/key_detail.py
@@ -6,7 +6,7 @@ from odoo import models, fields, api
 class KeyDetail(models.Model):
 	_name = 'fuenc.station.key.detail'
 	
-	name = fields.Char(string='名字')
+	name = fields.Char(string='钥匙名称')
 	line_id = fields.Char(string='选择线路')
 	ascription_site_id = fields.Char(string='归属站点')
 	key_type_id = fields.Many2one('fuenc.station.key.type', string='钥匙类型')
@@ -17,16 +17,15 @@ class KeyDetail(models.Model):
 	# borrow_user_id = fields.Many2one('res.users',string='借用人')
 	
 	@api.model
-	def create1(self, vals):
+	def create(self, vals):
 		if vals.get('key_no'):
 			key_nos = vals.get('key_no').split('\n')
 			for key_no in key_nos:
 				if key_no:
 					vals['key_no'] = key_no
-					super(KeyDetail, self).create(vals)
-		
-		# 这里有问题
-		return super(KeyDetail, self).create(vals)
+					record = super(KeyDetail, self).create(vals)
+		return record
 	
+	@api.model
 	def get_data_test(self):
 		print('ok')
