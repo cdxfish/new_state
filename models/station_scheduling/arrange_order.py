6a040ef8851a5cd20f0f2daa1fed6affedd5d62a
yang ying <284038143@qq.com>
2018-09-11 19:21:50 +0800

    排班设置

--- models/station_scheduling/arrange_order.py
+++ models/station_scheduling/arrange_order.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import datetime
+
+import odoo.exceptions as msg
 from odoo import models, fields, api
 
 
@@ -20,5 +23,46 @@ class arrange_order(models.Model):
 
     @api.constrains('start_work_time','end_work_time')
     def compute_work_time(self):
+        start_work_time = datetime.datetime.strptime(self.start_work_time, '%Y-%m-%d %H:%M:%S') + datetime.timedelta(hours=8)
+        end_work_time = datetime.datetime.strptime(self.end_work_time, '%Y-%m-%d %H:%M:%S') + datetime.timedelta(hours=8)
 
-        return self
+        # seconds
+        if (end_work_time- start_work_time).days < 0:
+            end_time_select = '次日'
+        else:
+            end_time_select = '当日'
+        # format(float((end_work_time - start_work_time).seconds) / float((60 * 60)), '.2f')
+        work_time = round( (end_work_time - start_work_time).seconds / (60 * 60), 2)
+        self.work_time = str(work_time) + 'h'
+        self.time = '{}-{} ({})'.format(self.start_work_time[11:-3], self.end_work_time[11:-3], end_time_select)
+
+    def create_arrange_order(self):
+        return {
+            'name': '新增班次',
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'funenc_xa_station.arrange_order',
+            'context': self.env.context,
+            # 'flags': {'initial_mode': 'edit'},
+            'target': 'new',
+        }
+
+    def arrange_order_edit(self):
+        return {
+            'name': '班次详情编辑',
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'funenc_xa_station.arrange_order',
+            'context': self.env.context,
+            'flags': {'initial_mode': 'edit'},
+            'res_id': self.id,
+            'target': 'new',
+        }
+
+    def arrange_order_delete(self):
+
+        self.env['funenc_xa_station.arrange_order'].search([('id', '=', self.id)]).unlink()
+
+
