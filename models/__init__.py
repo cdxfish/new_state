371163a7341dc7178a602f1796c95b0b8ae6dfb7
wang <wang@gmail.com>
2018-10-12 10:06:43 +0800

    冲突

--- models/__init__.py
+++ models/__init__.py
@@ -15,8 +15,6 @@ from . import check_attendance
 from . import Good_deeds
 from . import consumables_manage
 from . import prude_newspaper
-<<<<<<< HEAD
 from . import break_logo
-=======
 from . import production_manage
->>>>>>> b1adc69f56e09fd8ec03d7579e12774c57e89c4a
+
