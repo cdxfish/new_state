ae8ad1ad337204683a5148b25fcd32b8c44110ae
Artorias <305111632@qq.com>
2018-10-19 18:14:11 +0800

    add

--- models/staffing/position_settings.py
+++ models/staffing/position_settings.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import odoo.exceptions as msg
+from collections import defaultdict
 
 from odoo import models, fields, api
 
@@ -43,16 +44,70 @@ class PositionSettings(models.Model):
         if open_type == 'add':
             self.create({'name': group_name, 'category_id': category_id.id, 'implied_ids': [(6, 0, implied_ids)]})
         elif open_type == 'update':
+            now_implied_ids = self.browse(group_id).implied_ids.ids
+            print(now_implied_ids)
+            print(implied_ids)
             self.browse(group_id).write({'implied_ids': [(6, 0, implied_ids)]})
+            # 删除group中用户
 
-    def create_position_settings_button(self):
-        return {
-            'name': '职位创建',
-            'type': 'ir.actions.act_window',
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_model': 'res.groups',
-            'context': self.env.context,
-            # 'flags': {'initial_mode': 'edit'},
-            'target': 'new',
-        }
+    @api.model
+    def del_group_users(self, group_id, del_groups_ids, del_users_ids):
+        '''
+        清除对应组的人员
+        '''
+        groups = self.env['res.groups'].search(
+            [('id', '!=', group_id),
+             ('category_id', '=', self.env.ref('{}.{}'.format(MODULE_NAME, 'module_category_custom_groups')).id)])
+        del_group_user_map = defaultdict(list)
+        for item in del_groups_ids:
+            del_group_user_map[item] = del_users_ids
+        for group in groups:
+            cur_group_users = group.users.ids  # 当前分组的用户
+            deal_cur_groups_ids = list(set(group.implied_ids.ids).intersection(
+                set(del_groups_ids)))  # 当前分组需处理的子分组
+            for del_group_id in deal_cur_groups_ids:
+                del_group_user_map[del_group_id] = list(
+                    set(del_group_user_map[del_group_id]).difference(set(cur_group_users)))
+        for group_id in del_group_user_map:
+            if del_group_user_map[group_id]:
+                records = self.env['res.groups'].browse(group_id)
+                users_ids = list(set(records.users.ids).difference(
+                    set(del_group_user_map[group_id])))
+                records.write({
+                    'users': [(6, 0, users_ids)]
+                })
+
+    @api.multi
+    def write(self, vals):
+        '''
+        重写write，避免子组变化时，对应子组的人员的人员还具体原子组权限
+        '''
+        if 'implied_ids' in vals:  # 模块启动时也加载了此函数处理
+            del_groups_ids = self.implied_ids.ids  # 修改之前关联的分组
+            # 有组被删除时，手动处理
+            if not set(self.implied_ids.ids).issubset(vals['implied_ids'][0][2]):
+                del_groups_ids = list(set(del_groups_ids).difference(
+                    set(vals['implied_ids'][0][2])))
+                self.del_group_users(self.id, del_groups_ids, self.users.ids)
+        if 'users' in vals and vals['users']:  # 删除父组的人时，自动去除子组的人
+            # 有人被删除时，手动处理
+            if not set(self.users.ids).issubset(vals['users'][0][2]):
+                del_users_ids = list(
+                    set(self.users.ids).difference(set(vals['users'][0][2])))
+                self.del_group_users(
+                    self.id, self.implied_ids.ids, del_users_ids)
+        return super().write(vals)
+
+    @api.multi
+    def unlink(self):
+        '''
+        当前组的子组中人的删除处理：
+        若其他组的子组中包含该子组，且当前组的子组中包含的人被被包含在该子组中，则不能删除，否则，删除
+        '''
+        for record in self:
+            del_groups_ids = record.implied_ids.ids  # 需要删除的分组的
+            self.del_group_users(record.id, del_groups_ids, record.users.ids)
+            super(models.Model, record).unlink()
+
+        return True
+
