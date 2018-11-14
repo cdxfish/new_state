9922307a9eb5e840e7075f61e2ee48a435c34d8d
yang ying <284038143@qq.com>
2018-11-14 18:40:40 +0800

    交接班

--- models/station_scheduling/arrange_class_manage.py
+++ models/station_scheduling/arrange_class_manage.py
@@ -2,19 +2,31 @@
 
 from odoo import models, fields, api
 
+
 class ArrangeClassManage(models.Model):
     _name = 'funenc_xa_station.arrange_class_manage'
     _description = u'排班规则管理'
     _inherit = 'fuenc_station.station_base'
 
-    name = fields.Char(string='名称')
-    arrange_class_type = fields.Many2many('funenc_xa_station.arrange_order','arrange_class_manage_arrange_order_1_ref',
-                                          'arrange_class_manage_id', 'arrange_order_id',string='排班类型', required=True)
-    arrange_class_obj = fields.Many2one('funenc_xa_station.class_group', string='对象', required=True)  # 班组对象
-    remarks= fields.Text(string='备注')
+    name = fields.Char(string='名称', compute='_compute_name')
 
+    obj_selection = fields.Selection(selection=[('arrange_class', '班组'), ('motorized', '机动人员')],
+                                     default="arrange_class")  # 班组对象
+    remarks = fields.Text(string='备注')
 
+    order_to_arrange_ids = fields.One2many('arrange_order_to_arrange_class_manage', 'arrange_class_manage_id',
+                                           string='排班类型', required=True)
 
+    def _compute_name(self):
+        for this in self:
+            name = ''
+            for i, order_to_arrange_id in enumerate(this.order_to_arrange_ids):
+                if i == 0:
+                    name = name + order_to_arrange_id.arrange_order_id.name
+
+                else:
+                    name = name + ',' + order_to_arrange_id.arrange_order_id.name
+            this.name = name
 
     @api.model
     def create_arrange_class_manage(self):
@@ -46,3 +58,10 @@ class ArrangeClassManage(models.Model):
         # 还差在使用不能删除的判断
         self.unlink()
 
+
+class ArrangeOrderToArrangeClassManage(models.Model):
+    _name = 'arrange_order_to_arrange_class_manage'
+    _description = '班次和排班规则中间表'
+
+    arrange_class_manage_id = fields.Many2one('funenc_xa_station.arrange_class_manage', string='排班规则')
+    arrange_order_id = fields.Many2one('funenc_xa_station.arrange_order', string='排班班次')
