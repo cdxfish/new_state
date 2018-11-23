8846781b156e59200a22403d8f7e03233ca6d79e
merchantfield@163.com <619851623@qq.com>
2018-11-23 17:31:36 +0800

    update

--- models/add_management/models.py
+++ models/add_management/models.py
@@ -11,7 +11,7 @@ import os
 class xian_metro(models.Model):
     _name = 'xian_metro.xian_metro'
     # _inherit = 'fuenc_station.station_base'
-    line_id = fields.Many2one('cdtct_dingtalk.cdtct_dingtalk_department',string='线路')
+    line_id = fields.Many2many('cdtct_dingtalk.cdtct_dingtalk_department','line_id_site_id_ref','line_id','site_id',string='线路',)
     site_id = fields.Many2many('cdtct_dingtalk.cdtct_dingtalk_department', 'xian_metro_site_id_rel','xian_metro_id','site_id',string='站点')
     profession_kind = fields.Many2one('xian_metro.professional', string='专业分类')
     rank_kind = fields.Many2one('add_class.add_class', string='级别分类')
@@ -26,6 +26,42 @@ class xian_metro(models.Model):
     url = fields.Char(string='url')
     release_time = fields.Date(string='发布实施日期')
     rule_regulations_browse = fields.Selection([('one','内容显示'),('zero','内容不显示')],default='zero')
+    line_id_default = fields.Char(string='线路')
+    site_id_default = fields.Char(string='线路')
+
+    @api.onchange('line_id')
+    def change_line_id(self):
+        lis = []
+        if not self.line_id:
+            self.site_id = ''
+        else:
+            line_id = self.line_id
+            for i in line_id:
+                lis.append(i.departmentId)
+
+            child_department_ids = self.env['cdtct_dingtalk.cdtct_dingtalk_department'].search(
+                [('parentid', 'in', lis)]).ids
+            site_domain = [('id', 'in', list(set(child_department_ids)))]
+
+            return {'domain': {'site_id': site_domain,
+                               }
+                    }
+
+
+
+    def wrapper(self, *args, **kwargs):
+        ding_user = self.env.user.dingtalk_user
+        department_ids = ding_user.user_property_departments
+        line_ids = []
+        for department_id in department_ids:
+            if department_id.department_hierarchy == 3:
+                id = self.env['cdtct_dingtalk.cdtct_dingtalk_department'].search(
+                    [('departmentId', '=', department_id.parentid)]).id
+
+                line_ids.append(id)
+
+        return line_ids
+        print(line_ids)
 
     # 自动获取操作人的姓名
     @api.model
@@ -67,11 +103,25 @@ class xian_metro(models.Model):
     # 用来展示文件的样式
     @api.model
     def create(self, params):
+        lis_line = []
+        lis_site =[]
         file_binary = params['details']
+        for i in params.get('line_id'):
+            for line in i[2]:
+                char_line = self.env['cdtct_dingtalk.cdtct_dingtalk_department'].search([('id','=',line)])
+                lis_line.append(char_line.name)
+        params['line_id_default'] = "/".join(lis_line)
+        print(params['line_id_default'])
+        for i_site in params.get('site_id'):
+            for site in i_site[2]:
+                char_line = self.env['cdtct_dingtalk.cdtct_dingtalk_department'].search([('id', '=', site)])
+                lis_site.append(char_line.name)
+        params['site_id_default'] = "/".join(lis_site)
+        print(lis_site)
         file = os.path.splitext(params.get('file_name'))
         filename,type = file
-        if type != '.pdf':
-            raise ValidationError('上传的文件需要时pdf文件，请重新改选择')
+        # if type != '.pdf':
+        #     raise ValidationError('上传的文件需要时pdf文件，请重新改选择')
         file_name = params.get('file_name', self.file_name)
         if file_binary:
             url = self.env['qiniu_service.qiniu_upload_bucket'].upload_data(
