22e2562530b25e514d527adadbb110bbc8c6e010
merchantfield@163.com <619851623@qq.com>
2018-12-07 15:33:20 +0800

    汇总页面导出

---
+++ controllers/consumables_sumarry.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import json
+import time
+import os
+import xlrd
+import xlutils3.copy as xcopy
+import random
+from odoo import http
+from odoo.http import request
+import logging
+import base64
+
+APP_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+class BelongManagemnet(http.Controller):
+    @http.route('/funenc_xa_station/sumarry_summary', auth='public', csrf=False, type='http')
+    def import_excel_belong_to_management(self, **kw):
+        path = APP_DIR + '/static/excel/'
+        # arr_data = kwargs['arr_data']
+        json_data = json.loads(request.httprequest.data.decode())
+        arr_data = json_data['reserves']
+        # 打开模板excel文件进行读写操作
+        rdbook = xlrd.open_workbook(path + 'consumables_summary.xls')
+        # 复制模板
+        wtbook = xcopy.copy(rdbook)
+        worksheet = wtbook.get_sheet(0)
+
+        # 表头
+        row = 1
+        if len(arr_data) > 0:
+            for record in arr_data:
+                worksheet.write(row, 0, record['consumables_type'])
+                worksheet.write(row, 1, record['inventory'])
+                row += 1
+
+        name = str(int(round(time.time() * 1000))) + str(random.randint(1, 1000)) + '.xls'
+        file = path + name
+        wtbook.save(file)
+
+        with open(file, 'rb') as f:
+            data = f.read()
+        os.remove(file)
+        return data
--- models/integrated_management/station_equipment_summary.py
+++ models/integrated_management/station_equipment_summary.py
@@ -9,3 +9,35 @@ class StationEquipmentSummary(models.Model):
     def init_methods_action(self):
         return
 
+    @api.model
+    #获取所有车站设备的信息
+    def get_equipment_name(self):
+        lis = []
+        all_record = self.env['funenc_xa_station.station_equipment'].search([])
+        for record in all_record:
+            dic = {}
+            dic['equipment_fname']=record.name
+
+            dic['equipment_count']=record.count
+            lis.append(dic)
+
+        return lis
+
+    @api.model
+    #获取所有的设备名称的集合
+    def get_name_all(self):
+        lis = []
+        all_record = self.env['funenc_xa_station.station_equipment'].search([])
+        for record in all_record:
+            dic = {}
+            dic['name']=record.name
+
+            dic['id']=record.count
+            lis.append(dic)
+
+        return lis
+
+
+
+
+
