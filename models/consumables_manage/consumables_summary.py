feda0547b6173dc23e7cde4ab7f0497a1a73c127
yang ying <284038143@qq.com>
2018-12-07 16:00:17 +0800

    耗材

--- models/consumables_manage/consumables_summary.py
+++ models/consumables_manage/consumables_summary.py
@@ -64,38 +64,18 @@ class ConsumablesSummary(models.Model):
         return lis
 
     @api.model
-    def search_record_consumables(self,department,line,site,options):
+    def search_record_consumables(self,**kw):
         lis= []
-        op = [opt for opt in options if type(opt).__name__ == 'list']
+        if kw.get('department'):
+            dic_department_id = {1: kw.get('department')}  #部门
+        if kw.get('line'):
+            dic_line_id = {2: kw.get('line') } # 线路
+        if kw.get('site'):
+            dic_line_id = {3: kw.get('line')} # 站点
 
+        
 
         record = self.env['funenc_xa_station.consumables_inventory'].search([('consumables_type','=',2)])
-        sure_list = []
-        record_count = 0
-        for re in record:
-            if re.consumables_type.consumables_type not in sure_list:
-                dic = {}
-                dic['consumables_type'] = re.consumables_type.consumables_type
-                for count in record:
-                    record_count += count.inventory_count
-                dic['inventory'] = record_count
-                sure_list.append(re.consumables_type.consumables_type)
-                lis.append(dic)
         return lis
 
 
-        # record = self.env['funenc_xa_station.consumables_type'].search([])
-        # for i in record:
-        #     count_one = 0
-        #     count = self.env['funenc_xa_station.consumables_inventory'].search([("consumables_type", "=", i.id)])
-        #     site_record = self.env['funenc_xa_station.consumables_inventory'].search(
-        #                                     [("inventory_department_id", "=", site)])
-        #     for site_id in site_record:
-        #         dic = {}
-        #         site_id.consumables_type.consumables_type
-        #     for count_self in count:
-        #         count_one += count_self.inventory_count
-        #     dic['inventory'] = count_one
-        #     dic['consumables_type'] = i.consumables_type
-        #     lis.append(dic)
-        # return lis
