0a917fbbc555c0aec733f2aa76c12f8e46cad181
yang ying <284038143@qq.com>
2018-12-26 19:30:00 +0800

    定时更新钉钉

---
+++ models/update_ding_user_task.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from odoo import models, api
+
+
+class update_ding_user_task(models.Model):
+    _name = 'funenc_xa_station.update_ding_user_task'
+    _description = u'同步人钉钉任务'
+
+    @api.model
+    def update_ding_user_task(self):
+        self.env['cdtct_dingtalk.cdtct_dingtalk_account'].search([])[0].sync_dingtalk()
+        print('111')
